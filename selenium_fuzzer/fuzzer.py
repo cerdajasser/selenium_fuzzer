f360da17cf3128b1318635ff3d67f6424e8ee2f2
cerdajasser <cerdajasser@gmail.com>
2024-11-23 13:48:08 -0500

    I've fixed the syntax error by completing the argparse argument for --fuzz-fields and the main method. This should resolve the SyntaxError

--- selenium_fuzzer/fuzzer.py
+++ selenium_fuzzer/fuzzer.py
@@ -241,4 +241,16 @@ class Fuzzer:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Selenium Fuzzer Script")
     parser.add_argument("url", help="URL to fuzz")
-    parser.add_argument("--fuzz-fields", action="store_true", help
+    parser.add_argument("--fuzz-fields", action="store_true", help="Fuzz input fields")
+    parser.add_argument("--click-elements", action="store_true", help="Click through clickable elements")
+    parser.add_argument("--delay", type=int, default=1, help="Delay between actions in seconds")
+
+    args = parser.parse_args()
+
+    fuzzer = Fuzzer(args.url)
+
+    if args.fuzz_fields:
+        fuzzer.run_fuzz_fields(delay=args.delay)
+
+    if args.click_elements:
+        fuzzer.run_click_elements(delay=args.delay)
