b422c80462a0b527e714dc30d5be2cf08504bbca
cerdajasser <cerdajasser@gmail.com>
2024-11-24 13:38:15 -0500

    add timeout exception import to imput_detector.py

--- selenium_fuzzer/input_detector.py
+++ selenium_fuzzer/input_detector.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException  # Add this import
 from selenium_fuzzer.exceptions import ElementNotFoundError
 import time
 from typing import List, Dict
