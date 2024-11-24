682ce99391974509ad7a2d3dfa5d4fe9350756ec
cerdajasser <cerdajasser@gmail.com>
2024-11-24 09:54:30 -0500

    fixed big where --headless option was not being passed

--- selenium_fuzzer/selenium_driver.py
+++ selenium_fuzzer/selenium_driver.py
@@ -1,25 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium_fuzzer.config import Config
 
-def create_driver() -> webdriver.Chrome:
-    """
-    Create and return a configured Selenium WebDriver instance.
-    """
+def create_driver(headless: bool = False):
+    """Create and configure a Selenium WebDriver instance."""
     options = Options()
-    
-    # Set headless mode based on configuration
-    if Config.SELENIUM_HEADLESS:
-        options.add_argument('--headless')
-    
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--ignore-certificate-errors')
+    if headless:
+        options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    # Initialize ChromeDriver
-    service = Service(executable_path=Config.CHROMEDRIVER_PATH)
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.implicitly_wait(10)
+    driver = webdriver.Chrome(options=options)
     return driver
