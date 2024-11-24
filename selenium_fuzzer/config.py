246cea2dc5b5adc28d165feaa04df9947691272c
cerdajasser <cerdajasser@gmail.com>
2024-11-24 10:27:57 -0500

    fixed a loop

--- selenium_fuzzer/config.py
+++ selenium_fuzzer/config.py
@@ -1,21 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium_fuzzer.config import Config
+import os
 
-def create_driver(headless: bool = False):
-    """Create and configure a Selenium WebDriver instance."""
-    options = Options()
-    if headless:
-        options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    # Use the ChromeDriver path from config.py
-    driver_path = Config.CHROMEDRIVER_PATH
-    service = Service(executable_path=driver_path)
-
-    driver = webdriver.Chrome(service=service, options=options)
-    return driver
+class Config:
+    """Configuration settings for the selenium fuzzer."""
+    CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVER_PATH', '/usr/bin/chromedriver')
+    SELENIUM_HEADLESS = os.getenv('SELENIUM_HEADLESS', 'True') == 'True'
+    LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+    LOG_FILE = os.getenv('LOG_FILE', 'selenium_fuzzer.log')
