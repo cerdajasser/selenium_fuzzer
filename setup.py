714dc2a38c71a44441bf8030d81bf2154c7d5b7f
cerdajasser <cerdajasser@gmail.com>
2024-11-24 22:39:11 -0500

    verion 0.0.1 - good base to start - initial fuzzing works and field is found and fuzzed correctly

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='selenium_fuzzer',
-    version='1.0.0',
+    version='0.0.1',
     author='Jasser Cerda',
     author_email='cerdajasser@gmail.com',
     description='A Selenium-based fuzzer for input fields in Angular Material applications.',
