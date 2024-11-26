a9c3894495c044e8b25d2fa82fa8fb84310645f6
cerdajasser <cerdajasser@gmail.com>
2024-11-25 22:12:44 -0500

    removed the unnecessary imports to resolve the circular dependency issue.

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium_fuzzer.fuzzer import Fuzzer
 from selenium_fuzzer.utils import generate_safe_payloads
 from selenium_fuzzer.config import Config
 from selenium_fuzzer.js_change_detector import JavaScriptChangeDetector
+from selenium_fuzzer.fuzzer import Fuzzer
 import time
 
 def main():
--- selenium_fuzzer/__init__.py
+++ selenium_fuzzer/__init__.py
@@ -1,4 +1,3 @@
-from .fuzzer import Fuzzer
 from .input_detector import InputDetector
 from .click_analyzer import ClickAnalyzer
 from .unhider import Unhider
