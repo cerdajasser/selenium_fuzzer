48099f4f161972f4f01f05fa8d8af260a6efbddc
cerdajasser <cerdajasser@gmail.com>
2024-11-26 11:41:04 -0500

    turned on debug in config.py

--- selenium_fuzzer/config.py
+++ selenium_fuzzer/config.py
@@ -4,5 +4,5 @@ class Config:
     """Configuration settings for the selenium fuzzer."""
     CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVER_PATH', '/usr/bin/chromedriver')
     SELENIUM_HEADLESS = os.getenv('SELENIUM_HEADLESS', 'True') == 'True'
-    LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+    LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
     LOG_FILE = os.getenv('LOG_FILE', 'selenium_fuzzer.log')
