8e1b25593e61c62ee055894e7d005e1723ddaeee
cerdajasser <cerdajasser@gmail.com>
2024-11-29 21:35:20 -0500

    re adding config import

--- selenium_fuzzer/selenium_driver.py
+++ selenium_fuzzer/selenium_driver.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+from selenium_fuzzer.config import Config  # Correctly import the Config class
 
 def create_driver(headless: bool = False):
     """Create and configure a Selenium WebDriver instance with logging preferences."""
