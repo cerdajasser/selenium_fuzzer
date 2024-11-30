eeebf7c108e96d8a3688b957b447e44e8bfacf37
cerdajasser <cerdajasser@gmail.com>
2024-11-29 20:48:59 -0500

    Removed desired_capabilities argument:
    The recent versions of Selenium require the capabilities to be passed through the options object or handled in a different manner.
    Updated the driver instantiation to use options and capabilities:
    
    To make sure that console logging works correctly, DesiredCapabilities.CHROME.copy() is used and logging preferences are set up using goog:loggingPrefs.
    Combining options and capabilities properly:
    
    In the updated version of Selenium, you can just provide the capabilities through options and avoid using desired_capabilities directly.

--- selenium_fuzzer/selenium_driver.py
+++ selenium_fuzzer/selenium_driver.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-from selenium_fuzzer.config import Config  # Import the Config class
+from selenium_fuzzer.config import Config  # Import Config for driver path
 
 def create_driver(headless: bool = False):
     """Create and configure a Selenium WebDriver instance with logging preferences."""
     options = Options()
+    
     if headless:
         options.add_argument("--headless")
     options.add_argument("--disable-gpu")
@@ -14,19 +15,14 @@ def create_driver(headless: bool = False):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    # Enable browser logging to capture all logs (removing the problematic 'desired_capabilities' issue)
+    # Set up Chrome logging preferences in capabilities
     capabilities = DesiredCapabilities.CHROME.copy()
-    capabilities["goog:loggingPrefs"] = {
-        "browser": "ALL",
-        "performance": "ALL",
-        "driver": "ALL"
-    }
+    capabilities["goog:loggingPrefs"] = {"browser": "ALL"}  # Enable console logging
 
-    # Use the ChromeDriver path from Config
+    # Use the ChromeDriver path from config.py
     driver_path = Config.CHROMEDRIVER_PATH
     service = Service(executable_path=driver_path)
 
-    # Combine options with the service and capabilities to create the WebDriver
+    # Pass capabilities through options
     driver = webdriver.Chrome(service=service, options=options, desired_capabilities=capabilities)
-    
     return driver
